cb8a0b1ed5b0a1d6992eac2496e862a6dda6b36a
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-11-05 21:46:34 -0800

    fix case sensitivity

---
+++ scripts/main.gd
@@ -0,0 +1,53 @@
+extends Node
+
+@export var mob_scene: PackedScene = preload("res://scenes/Mob.tscn")
+var time
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func game_over():
+	$ElapsedTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
+
+func new_game():
+	get_tree().call_group("mobs", "queue_free") # Clear mobs
+	get_tree().call_group("projectiles", "queue_free") # Clear projectiles
+	time = 0
+	$Player.start($StartPosition.position)
+	$HUD.update_score(time)
+	$HUD.show_message("Get Ready")
+	$StartTimer.start()
+
+func _on_elapsed_timer_timeout():
+	time += 1
+	$HUD.update_score(time)
+
+func _on_start_timer_timeout():
+	$MobTimer.start()
+	$ElapsedTimer.start()
+
+func _on_mob_timer_timeout():
+	# Create a new instance of the Mob scene.
+	var mob = mob_scene.instantiate()
+	mob.set_player($Player)
+
+	# Choose a random location on Path2D.
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	mob_spawn_location.progress_ratio = randf()
+
+	# Set the mob's position to a random location.
+	mob.position = mob_spawn_location.position
+
+	# Spawn the mob by adding it to the Main scene.
+	add_child(mob)
+
+
+func _on_star_timer_timeout():
+	$Player.shoot()
